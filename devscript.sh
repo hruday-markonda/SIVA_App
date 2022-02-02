468361b323a7867e9a4363f20731ea00b603558c
hruday-markonda <upishcat369@gmail.com>
2022-02-03 01:41:19 +1100

    made a helpful script for developers

--- devscript.sh
+++ devscript.sh
@@ -1,12 +1,23 @@
-arg=$1
+#!/bin/bash
 
-pip3 install pyinstaller
-pip3 install SpeechRecognition
-pip3 install beautifulsoup4
-pip3 install inflect
-pip3 install pywebview
-pip3 install PyAudio
-pip3 install Flask
-pip3 install urllib3
-pip3 install setuptools
-pip3 install Werkzeug
+if [ $1 = dependent ]
+then
+    pip3 install pyinstaller
+    pip3 install SpeechRecognition
+    pip3 install beautifulsoup4
+    pip3 install inflect
+    pip3 install pywebview
+    pip3 install PyAudio
+    pip3 install Flask
+    pip3 install urllib3
+    pip3 install setuptools
+    pip3 install Werkzeug
+
+elif [ $1 = compile ]
+then
+    cd ./Source
+    pyinstaller -w -F --hidden-import=pkg_resources.py2_warn --add-data "templates:templates" --add-data "static:static" --add-data "SIVAintents.json:." --add-data "SivaActions/PyJsVarDump.json:." --add-data "EnglishStopWords.txt:." --name SIVA --icon icon.icns InitSIVA.py
+
+else
+echo 'Invalid Argument'
+fi
